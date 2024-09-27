780c744fcef316b2d647eb7cbe916072dbb526b2
Teddy Wing <git@teddywing.com>
2024-09-27 18:30:03 +0200

    Go to selected search result on Enter
    When pressing Enter in the AODocs Knowledge Base search field, go to the
    highlighted article rather than triggering a non-Algolia Zendesk page
    load search.

---
+++ aodocs-kb-search-enter.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name AODocs KB Search Enter
+// @description Fix <Enter> key in AODocs Knowledge Base search field autocompletion
+// @version 0.0.1
+// @namespace com.teddywing
+// @match https://support.aodocs.com/*
+// ==/UserScript==
+
+var search_input = document.getElementById('query');
+
+search_input.addEventListener(
+	'keydown',
+	function(event) {
+		if (
+			event.key !== 'Enter'
+			|| (event.altKey && event.key === 'Enter')
+		) {
+			return;
+		}
+
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+		event.preventDefault();
+
+		go_to_highlighted_article();
+	}
+);
+
+function go_to_highlighted_article () {
+	var algolia_autocomplete_dropdown = document.querySelector(
+		'.algolia-autocomplete .aa-dataset-articles'
+	);
+
+	// Check content of search drop-down:
+	// console.log(algolia_autocomplete_dropdown.innerHTML);
+
+	var highlighted_autocompletion = algolia_autocomplete_dropdown.querySelectorAll(
+		'.aa-suggestion[aria-selected="true"]'
+	);
+
+	console.log('###', highlighted_autocompletion);
+
+	highlighted_autocompletion.click();
+
+	// window.location.replace('');
+}
