609a9ccdf4e77e5890ed05209abd0ca8a608fc86
Teddy Wing <git@teddywing.com>
2026-08-02 00:54:32 +0200

    Add license (GNU GPLv3+)

--- aodocs-kb-search-enter.user.js
+++ aodocs-kb-search-enter.user.js
@@ -6,6 +6,21 @@
 // @match https://support.aodocs.com/*
 // ==/UserScript==
 
+// Copyright (c) 2024  Teddy Wing
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 var SCRIPT_NAME = 'AODocs KB Search Enter';
 
 var search_input = document.getElementById('query');
